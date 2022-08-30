4fb6e0898a2671359b5e4be380ed738771169b81
sfmth <sfmth0@gmail.com>
2022-08-30 23:06:22 +0430

    doing 5 bit cordic

--- src/config.tcl
+++ src/config.tcl
@@ -20,10 +20,10 @@ $::env(DESIGN_DIR)/scan_wrapper_019235602376235615.v"
 
 # absolute die size
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 150 150"
+set ::env(DIE_AREA) "0 0 100 100"
 set ::env(FP_CORE_UTIL) 45
 set ::env(PL_BASIC_PLACEMENT) {1}
-set ::env(PL_TARGET_DENSITY) 0.65
+# set ::env(PL_TARGET_DENSITY) 0.65
 
 # use alternative efabless decap cells to solve LI density issue
 set ::env(DECAP_CELL) "\
--- src/user_module_019235602376235615.v
+++ src/user_module_019235602376235615.v
@@ -20,11 +20,11 @@ module user_module_019235602376235615(
 
     wire clk = io_in[0];
     wire reset = io_in[1];
-    wire [5:0] z0 = io_in[7:2]; // input angle supports from -90 to +90 degrees
+    wire [4:0] z0 = io_in[6:2]; // input angle supports from -90 to +90 degrees
 
     // ROM
-    reg [5:0]x0;
-    reg [5:0]angle[5:0];
+    reg [4:0]x0;
+    reg [4:0]angle[5:0];
     always @(posedge clk) begin
         x0 <= 6'd19; // x0 = 0.60728
         angle[0] <= 6'd16; // angle 0 = 45
@@ -32,11 +32,11 @@ module user_module_019235602376235615(
         angle[2] <= 6'd5; // angle 2 = 14.036
         angle[3] <= 6'd2; // angle 3 = 7.125
         angle[4] <= 6'd1; // angle 4 = 3.576
-        angle[5] <= 6'd1; // angle 5 = 1.7899
+        /* angle[5] <= 6'd1; // angle 5 = 1.7899 */
     end
 
     // Registers 
-    reg [5:0] reg_x, reg_y, reg_x_next, reg_y_next, reg_z, reg_z_next;
+    reg [4:0] reg_x, reg_y, reg_x_next, reg_y_next, reg_z, reg_z_next;
     reg en;
     always @(posedge clk) begin
         if (reset) begin
@@ -52,7 +52,7 @@ module user_module_019235602376235615(
 
     // ALU
     wire d;
-    assign d = reg_z[5];
+    assign d = reg_z[4];
     always @(*) begin
         case (i)
             3'd0: begin
@@ -68,63 +68,63 @@ module user_module_019235602376235615(
             end
             3'd1: begin
                 if (d) begin // if d then x is add and y is sub and z is add
-                    reg_x_next <= reg_x + reg_y[5:1]; // fix this
-                    reg_y_next <= reg_y - reg_x[5:1];
+                    reg_x_next <= reg_x + reg_y[4:1]; // fix this
+                    reg_y_next <= reg_y - reg_x[4:1];
                     reg_z_next <= reg_z + angle[1];
                 end else begin // if !d then x is sub and y is add and z is sub
-                    reg_x_next <= reg_x - reg_y[5:1];
-                    reg_y_next <= reg_y + reg_x[5:1];
+                    reg_x_next <= reg_x - reg_y[4:1];
+                    reg_y_next <= reg_y + reg_x[4:1];
                     reg_z_next <= reg_z - angle[1];
                 end
             end
             3'd2: begin
                 if (d) begin // if d then x is add and y is sub and z is add
-                    reg_x_next <= reg_x + reg_y[5:2];
-                    reg_y_next <= reg_y - reg_x[5:2];
+                    reg_x_next <= reg_x + reg_y[4:2];
+                    reg_y_next <= reg_y - reg_x[4:2];
                     reg_z_next <= reg_z + angle[2];
                 end else begin // if !d then x is sub and y is add and z is sub
-                    reg_x_next <= reg_x - reg_y[5:2];
-                    reg_y_next <= reg_y + reg_x[5:2];
+                    reg_x_next <= reg_x - reg_y[4:2];
+                    reg_y_next <= reg_y + reg_x[4:2];
                     reg_z_next <= reg_z - angle[2];
                 end
             end
             3'd3: begin
                 if (d) begin // if d then x is add and y is sub and z is add
-                    reg_x_next <= reg_x + reg_y[5:3];
-                    reg_y_next <= reg_y - reg_x[5:3];
+                    reg_x_next <= reg_x + reg_y[4:3];
+                    reg_y_next <= reg_y - reg_x[4:3];
                     reg_z_next <= reg_z + angle[3];
                 end else begin // if !d then x is sub and y is add and z is sub
-                    reg_x_next <= reg_x - reg_y[5:3];
-                    reg_y_next <= reg_y + reg_x[5:3];
+                    reg_x_next <= reg_x - reg_y[4:3];
+                    reg_y_next <= reg_y + reg_x[4:3];
                     reg_z_next <= reg_z - angle[3];
                 end
             end
             3'd4: begin
                 if (d) begin // if d then x is add and y is sub and z is add
-                    reg_x_next <= reg_x + reg_y[5:4];
-                    reg_y_next <= reg_y - reg_x[5:4];
+                    reg_x_next <= reg_x + reg_y[4];
+                    reg_y_next <= reg_y - reg_x[4];
                     reg_z_next <= reg_z + angle[4];
                 end else begin // if !d then x is sub and y is add and z is sub
-                    reg_x_next <= reg_x - reg_y[5:4];
-                    reg_y_next <= reg_y + reg_x[5:4];
+                    reg_x_next <= reg_x - reg_y[4];
+                    reg_y_next <= reg_y + reg_x[4];
                     reg_z_next <= reg_z - angle[4];
                 end
             end
-            3'd5: begin
-                if (d) begin // if d then x is add and y is sub and z is add
-                    reg_x_next <= reg_x + reg_y[5];
-                    reg_y_next <= reg_y - reg_x[5];
-                    reg_z_next <= reg_z + angle[5];
-                end else begin // if !d then x is sub and y is add and z is sub
-                    reg_x_next <= reg_x - reg_y[5];
-                    reg_y_next <= reg_y + reg_x[5];
-                    reg_z_next <= reg_z - angle[5];
-                end
-            end
+            /* 3'd5: begin */
+            /*     if (d) begin // if d then x is add and y is sub and z is add */
+            /*         reg_x_next <= reg_x + reg_y[5]; */
+            /*         reg_y_next <= reg_y - reg_x[5]; */
+            /*         reg_z_next <= reg_z + angle[5]; */
+            /*     end else begin // if !d then x is sub and y is add and z is sub */
+            /*         reg_x_next <= reg_x - reg_y[5]; */
+            /*         reg_y_next <= reg_y + reg_x[5]; */
+            /*         reg_z_next <= reg_z - angle[5]; */
+            /*     end */
+            /* end */
             default: begin
-                reg_x_next <= 6'bx;
-                reg_y_next <= 6'bx;
-                reg_z_next <= 6'bx;
+                reg_x_next <= 5'bx;
+                reg_y_next <= 5'bx;
+                reg_z_next <= 5'bx;
             end
         endcase    
     end
@@ -150,7 +150,7 @@ module user_module_019235602376235615(
                     
             end
             `CALC: begin
-                if ( i < 5) begin
+                if ( i < 4) begin
                     i <= i+1;
                     if (i == 4)
                         en <= 0;
@@ -178,8 +178,8 @@ module user_module_019235602376235615(
     wire x_yb;
     assign x_yb = clk;
     assign io_out[6] = x_yb;
-    wire [5:0] data_out;
-    assign io_out[5:0] = data_out;
+    wire [4:0] data_out;
+    assign io_out[4:0] = data_out;
 
     assign data_out = (done) ? (clk) ? reg_x : reg_y : 0;
 
